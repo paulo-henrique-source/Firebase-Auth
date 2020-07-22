57bc34c38e0f9e6c2bdbdff01830b2cabd96c50a
paulo-henrique-source <paulohenrique1117@hotmail.com>
2020-07-21 22:14:42 -0300

    Magic Wall2

--- views/authentication.js
+++ views/authentication.js
@@ -8,37 +8,37 @@ var inputPassword = document.getElementById('inputPassword')
 
 //Atutenticação
 btnLogin.addEventListener('click', function () {
-    firebase.auth().signInWithEmailAndPassword(inputEmail.value, inputPassword.value).then(function (result) {
-      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-      .then(function() {
-        return firebase.auth().signInWithEmailAndPassword(email, password)
-      })
-      
-      .catch(function(error) {
-        var errorCode = error.code
-        var errorMessage = error.message
-      })
-    alert("Usuário Autenticado!")
-    console.log(user)
-    })
+firebase.auth().signInWithEmailAndPassword(inputEmail.value, inputPassword.value).then(function (result) {
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+.then(function() {
+return firebase.auth().signInWithEmailAndPassword(email, password)
+})
 
-    .catch(function (error) {
-    var errorCode = error.code
-    var errorMessage = error.message
-    alert("Usuário não Autenticado!")
+.catch(function(error) {
+var errorCode = error.code
+var errorMessage = error.message
+})
+alert("Usuário Autenticado!")
+console.log(user)
+})
 
-    console.log("Failure!")
-    })
+.catch(function (error) {
+var errorCode = error.code
+var errorMessage = error.message
+alert("Usuário não Autenticado!")
+
+console.log("Failure!")
+})
 
 //LOGOUT    
 logOutButton.addEventListener('click', function () {
-     firebase
-     .auth()
-     .signOut()
-     .then(function () {
-      alert('Você deslogou')
-     }, function (error) {
-      console.error(error)
+firebase
+.auth()
+.signOut()
+.then(function () {
+alert('Você deslogou')
+}, function (error) {
+console.error(error)
 })
 })
 
